29cd1c55368ae2035f810d6796ed14f765c7ca6b
zeglarz <zeglarz@gmail.com>
2019-11-14 20:48:43 +0100

    Add methods delegation

--- src/main/java/com/mja/Registry.java
+++ src/main/java/com/mja/Registry.java
@@ -1,4 +1,19 @@
 package com.mja;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class Registry {
+
+    private UserBase userBase = UserBase.getInstance();
+    private SingUp singUp = new SingUp(userBase);
+
+    public void singUp(String login, String password, String name, String surname, int age){
+        String message = singUp.addUser(login, password, name, surname, age);
+        System.out.println(message);
+    }
+
+    public void printUserBase(){
+        userBase.toString();
+    }
 }
