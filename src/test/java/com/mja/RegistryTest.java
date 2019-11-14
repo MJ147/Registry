e608e78d607830f2f9c06c063f427a1500779908
zeglarz <zeglarz@gmail.com>
2019-11-14 20:50:00 +0100

    Add test

--- src/test/java/com/mja/RegistryTest.java
+++ src/test/java/com/mja/RegistryTest.java
@@ -4,11 +4,13 @@ public class RegistryTest {
 
     public static void main(String[] args) {
 
-        SingUP registry = new SingUP();
-        registry.singUp("User1", "password1", "name", "surname", 18);
-        registry.singUp("","", "","", 18);
+        Registry registry = new Registry();
+        registry.singUp("User1", "password1", "name1", "surname2", 18);
+        registry.singUp("User2", "password2", "name2", "surname2", 19);
+        registry.singUp("User3", "password3", "name3", "surname3", 20);
 
 
-        registry.toString();
+
+        registry.printUserBase();
     }
 }
