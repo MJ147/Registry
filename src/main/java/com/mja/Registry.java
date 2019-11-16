01deddf4cba2d16adcf2c08516065e7b7ab6ce89
Marek Jaczewski <jaczewski147@gmail.com>
2019-11-16 18:55:28 +0100

    Add getUserBase method

--- src/main/java/com/mja/Registry.java
+++ src/main/java/com/mja/Registry.java
@@ -1,5 +1,9 @@
 package com.mja;
 
+import com.mja.model.User;
+
+import java.util.Map;
+
 public class Registry {
 
     private UserBase userBase = UserBase.getInstance();
@@ -8,6 +12,10 @@ public class Registry {
     public void singUp(String login, String password, String name, String surname, int age){
         String message = singUp.addUser(login, password, name, surname, age);
         System.out.println(message);
+    }
+
+    public Map<String, User> getUserBase() {
+        return userBase.getUserMap();
     }
 
     public void printUserBase(){
