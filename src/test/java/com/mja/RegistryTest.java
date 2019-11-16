4c158a5c35d87582d2f6930279eaf0fe591b9af7
Marek Jaczewski <jaczewski147@gmail.com>
2019-11-16 18:52:44 +0100

    Add tests

--- src/test/java/com/mja/RegistryTest.java
+++ src/test/java/com/mja/RegistryTest.java
@@ -1,16 +1,35 @@
 package com.mja;
 
-public class RegistryTest {
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 
-    public static void main(String[] args) {
 
-        Registry registry = new Registry();
+class RegistryTest {
+
+    Registry registry;
+
+    @BeforeEach
+    void setUpUserBase() {
+        registry = new Registry();
         registry.singUp("User1", "password1", "name1", "surname2", 18);
         registry.singUp("User2", "password2", "name2", "surname2", 19);
         registry.singUp("User3", "password3", "name3", "surname3", 20);
-
-
-
-        registry.printUserBase();
     }
+
+    @Test
+    void notEmptyBaseTest() {
+        Assertions.assertFalse(registry.getUserBase().isEmpty());
+    }
+
+    @Test
+    void numberOfUserTest() {
+        Assertions.assertEquals(3, registry.getUserBase().size());
+    }
+
+    @Test
+    void singUpTest() {
+        Assertions.assertEquals("password3", registry.getUserBase().get("User3").getPassword());
+    }
+
 }
