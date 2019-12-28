bc5a2be004f2c78f6c02bb3d65e477f01c648a35
Marek Jaczewski <jaczewski147@gmail.com>
2019-12-28 16:55:58 +0100

    add signIn method

--- src/main/java/com/mja/Registry.java
+++ src/main/java/com/mja/Registry.java
@@ -2,22 +2,27 @@ package com.mja;
 
 import com.mja.database.UserBase;
 import com.mja.model.User;
-import com.mja.service.SignUp;
+import com.mja.service.UserService;
 
 import java.util.Map;
 
 public class Registry {
 
     private UserBase userBase;
-    private SignUp signUp;
+    private UserService userService;
 
     public Registry() {
         this.userBase = new UserBase();
-        this.signUp = new SignUp(userBase);
+        this.userService = new UserService(userBase);
     }
 
     public void signUp(String login, String password, String name, String surname, int age){
-        String message = signUp.addUser(login, password, name, surname, age);
+        String message = userService.addUser(login, password, name, surname, age);
+        System.out.println(message);
+    }
+
+    public void signIn(String login, String password) {
+        String message = userService.loginUser(login, password);
         System.out.println(message);
     }
 
